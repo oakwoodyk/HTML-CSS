1a5e80e9f81921a7c429ed33186daa6fb0aad3cb
oakwoodyk <yuka1217kimura86@gmail.com>
2021-10-02 22:14:46 +0900

    final-submit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
+const messege = document.getElementById("chat");
+messege.addEventListener("click", function () {
+  alert("わーいわーい！")
+})
+
+
 let img;
 function showdown() {
-  img = document.getElementByClassName("pic");
-  img.src = "img/down.png";
-}
+  img = document.getElementsByClassName("pic");
+  img[0].src = "img/down.png";
+};
+
+function showrun() {
+  img = document.getElementsByClassName("pic");
+  img[0].src = "img/run.png";
+};
+function showbaah() {
+  img = document.getElementsByClassName("pic");
+  img[0].src = "img/baba.png";
+};
+
+function showchange() {
+  img = document.getElementsByClassName("pic");
+  img[0].src = "img/change.png";
+};
+
+function showback() {
+  img = document.getElementsByClassName("pic");
+  img[0].src = "img/top.png";
+};
+
+
